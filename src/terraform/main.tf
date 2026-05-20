0de9a81064ef8791db09491b3b9de45573381e2d
patrick <patrickpk4@hotmail.com>
2026-05-20 12:58:48 -0300

    comenta eks para teste pipeline

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -25,45 +25,45 @@ module "vpc" {
 ########################### EKS #####################################
 
 
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
-
-  name               = var.kube_name
-  kubernetes_version = var.kube_version
-
-  addons = {
-    coredns = {}
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-    kube-proxy = {}
-    vpc-cni = {
-      before_compute = true
-    }
-  }
-
-  # Optional
-  endpoint_public_access = true
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      instance_types = var.instance_types
-
-      min_size     = 2
-      max_size     = 2
-      desired_size = 4
-    }
-  }
-
-  tags = var.project_tags
-}
+#
+#module "eks" {
+#  source  = "terraform-aws-modules/eks/aws"
+#  version = "~> 21.0"
+#
+#  name               = var.kube_name
+#  kubernetes_version = var.kube_version
+#
+#  addons = {
+#    coredns = {}
+#    eks-pod-identity-agent = {
+#      before_compute = true
+#    }
+#    kube-proxy = {}
+#    vpc-cni = {
+#      before_compute = true
+#    }
+#  }
+#
+#  # Optional
+#  endpoint_public_access = true
+#
+#  # Optional: Adds the current caller identity as an administrator via cluster access entry
+#  enable_cluster_creator_admin_permissions = true
+#
+#  vpc_id     = module.vpc.vpc_id
+#  subnet_ids = module.vpc.private_subnets
+#
+#  # EKS Managed Node Group(s)
+#  eks_managed_node_groups = {
+#    example = {
+#      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+#      instance_types = var.instance_types
+#
+#      min_size     = 2
+#      max_size     = 2
+#      desired_size = 4
+#    }
+#  }
+#
+#  tags = var.project_tags
+#} 
