0a147eddc4fc8d73e231c027c9c0bb76ca81205e
patrick <patrickpk4@hotmail.com>
2026-05-21 16:34:42 -0300

    refatora pipelane e estrutura de diretorios do projeto

---
+++ src/terraform/env/staging/provaider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.41.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
