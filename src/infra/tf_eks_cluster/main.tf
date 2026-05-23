367a722fc1e60fc716014cc4d2b835a050eba703
patrick <patrickpk4@hotmail.com>
2026-05-23 10:34:14 -0300

    tf: edit mais eks endpoint

--- src/infra/tf_eks_cluster/main.tf
+++ src/infra/tf_eks_cluster/main.tf
@@ -46,7 +46,6 @@ module "eks" {
 
   # Optional
   endpoint_public_access  = true
-  endpoint_private_access = true
 
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
