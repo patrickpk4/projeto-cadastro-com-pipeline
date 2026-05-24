5196e72eb232f2386801d2ed27ec1c1ffc4b3b57
patrick <patrickpk4@hotmail.com>
2026-05-24 08:56:30 -0300

    tf: edita main

--- src/infra/tf_eks_cluster/main.tf
+++ src/infra/tf_eks_cluster/main.tf
@@ -45,7 +45,7 @@ module "eks" {
   }
 
   # Optional
-  endpoint_public_access  = false
+  endpoint_public_access  = true
   endpoint_private_access = true
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
