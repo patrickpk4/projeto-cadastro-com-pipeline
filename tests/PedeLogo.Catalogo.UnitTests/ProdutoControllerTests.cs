423b32831dd69f4def14e4c2d831dd18416fcce9
patrick <patrickpk4@hotmail.com>
2026-06-01 17:49:48 -0300

    edit test

--- tests/PedeLogo.Catalogo.UnitTests/ProdutoControllerTests.cs
+++ tests/PedeLogo.Catalogo.UnitTests/ProdutoControllerTests.cs
@@ -1,4 +1,3 @@
-cat > tests/PedeLogo.Catalogo.UnitTests/ProdutoControllerTests.cs << 'EOF'
 using System.Collections.Generic;
 using System.Threading;
 using FluentAssertions;
@@ -25,16 +24,11 @@ namespace PedeLogo.Catalogo.UnitTests
             _loggerMock = new Mock<ILogger<ProdutoController>>();
             _dbMock = new Mock<IMongoDatabase>();
             _collectionMock = new Mock<IMongoCollection<Produto>>();
-
-            _dbMock
-                .Setup(db => db.GetCollection<Produto>("Produto", null))
-                .Returns(_collectionMock.Object);
-
+            _dbMock.Setup(db => db.GetCollection<Produto>("Produto", null)).Returns(_collectionMock.Object);
             _controller = new ProdutoController(_loggerMock.Object, _dbMock.Object);
         }
 
-        [Fact]
-        [Trait("Category", "Unit")]
+        [Fact][Trait("Category", "Unit")]
         public void Get_QuandoExistemProdutos_DeveRetornarLista()
         {
             var produtos = new List<Produto>
@@ -42,81 +36,84 @@ namespace PedeLogo.Catalogo.UnitTests
                 new Produto { Id = ObjectId.GenerateNewId().ToString(), Nome = "Pizza", Preco = 39.90, Categoria = "Comida" },
                 new Produto { Id = ObjectId.GenerateNewId().ToString(), Nome = "Refrigerante", Preco = 7.00, Categoria = "Bebida" }
             };
-
             var cursorMock = CriarCursorMock(produtos);
-            _collectionMock
-                .Setup(c => c.FindSync(
-                    It.IsAny<FilterDefinition<Produto>>(),
-                    It.IsAny<FindOptions<Produto, Produto>>(),
-                    It.IsAny<CancellationToken>()))
-                .Returns(cursorMock.Object);
-
+            _collectionMock.Setup(c => c.FindSync(It.IsAny<FilterDefinition<Produto>>(), It.IsAny<FindOptions<Produto, Produto>>(), It.IsAny<CancellationToken>())).Returns(cursorMock.Object);
             var resultado = _controller.Get();
-
             resultado.Should().HaveCount(2);
             resultado.Should().ContainSingle(p => p.Nome == "Pizza");
         }
 
-        [Fact]
-        [Trait("Category", "Unit")]
+        [Fact][Trait("Category", "Unit")]
         public void Get_QuandoNaoExistemProdutos_DeveRetornarListaVazia()
         {
             var cursorMock = CriarCursorMock(new List<Produto>());
-            _collectionMock
-                .Setup(c => c.FindSync(
-                    It.IsAny<FilterDefinition<Produto>>(),
-                    It.IsAny<FindOptions<Produto, Produto>>(),
-                    It.IsAny<CancellationToken>()))
-                .Returns(cursorMock.Object);
-
+            _collectionMock.Setup(c => c.FindSync(It.IsAny<FilterDefinition<Produto>>(), It.IsAny<FindOptions<Produto, Produto>>(), It.IsAny<CancellationToken>())).Returns(cursorMock.Object);
             var resultado = _controller.Get();
-
             resultado.Should().BeEmpty();
         }
 
-        [Fact]
-        [Trait("Category", "Unit")]
+        [Fact][Trait("Category", "Unit")]
         public void GetById_ComIdValido_DeveRetornarProduto()
         {
             var id = ObjectId.GenerateNewId().ToString();
             var produto = new Produto { Id = id, Nome = "Hamburguer", Preco = 25.00, Categoria = "Comida" };
-
             var cursorMock = CriarCursorMock(new List<Produto> { produto });
-            _collectionMock
-                .Setup(c => c.FindSync(
-                    It.IsAny<FilterDefinition<Produto>>(),
-                    It.IsAny<FindOptions<Produto, Produto>>(),
-                    It.IsAny<CancellationToken>()))
-                .Returns(cursorMock.Object);
-
+            _collectionMock.Setup(c => c.FindSync(It.IsAny<FilterDefinition<Produto>>(), It.IsAny<FindOptions<Produto, Produto>>(), It.IsAny<CancellationToken>())).Returns(cursorMock.Object);
             var resultado = _controller.Get(id);
-
             resultado.Should().NotBeNull();
             resultado.Nome.Should().Be("Hamburguer");
         }
 
-        [Fact]
-        [Trait("Category", "Unit")]
+        [Fact][Trait("Category", "Unit")]
         public void GetById_ComIdInvalido_DeveLancarException()
         {
-            var idInvalido = "id-que-nao-e-objectid";
-
-            var act = () => _controller.Get(idInvalido);
-
-            act.Should().Throw<System.Exception>()
-               .WithMessage("Erro ao converter.");
+            var act = () => _controller.Get("id-invalido");
+            act.Should().Throw<System.Exception>().WithMessage("Erro ao converter.");
         }
 
-        [Fact]
-        [Trait("Category", "Unit")]
+        [Fact][Trait("Category", "Unit")]
         public void Post_ComProdutoValido_DeveRetornarOk()
         {
             var produto = new Produto { Nome = "Sushi", Preco = 55.00, Categoria = "Japonesa" };
-
-            _collectionMock
-                .Setup(c => c.InsertOne(produto, null, default))
-                .Verifiable();
-
+            _collectionMock.Setup(c => c.InsertOne(produto, null, default)).Verifiable();
             var resultado = _controller.Post(produto);
+            resultado.Should().BeOfType<OkResult>();
+            _collectionMock.Verify(c => c.InsertOne(produto, null, default), Times.Once);
+        }
 
-            res
+        [Fact][Trait("Category", "Unit")]
+        public void Put_ComIdInvalido_DeveLancarException()
+        {
+            var act = () => _controller.Put("nao-e-objectid", new Produto { Nome = "Teste" });
+            act.Should().Throw<System.Exception>().WithMessage("Id errado");
+        }
+
+        [Fact][Trait("Category", "Unit")]
+        public void Put_ComIdValido_DeveChamarFindOneAndReplace()
+        {
+            var id = ObjectId.GenerateNewId().ToString();
+            var produto = new Produto { Id = id, Nome = "Pizza Atualizada", Preco = 45.00 };
+            _collectionMock.Setup(c => c.FindOneAndReplace(It.IsAny<FilterDefinition<Produto>>(), produto, It.IsAny<FindOneAndReplaceOptions<Produto, Produto>>(), It.IsAny<CancellationToken>())).Returns(produto);
+            var act = () => _controller.Put(id, produto);
+            act.Should().NotThrow();
+        }
+
+        [Fact][Trait("Category", "Unit")]
+        public void Delete_ComIdValido_DeveChamarFindOneAndDelete()
+        {
+            var id = ObjectId.GenerateNewId().ToString();
+            _collectionMock.Setup(c => c.FindOneAndDelete(It.IsAny<FilterDefinition<Produto>>(), It.IsAny<FindOneAndDeleteOptions<Produto, Produto>>(), It.IsAny<CancellationToken>())).Returns((Produto)null);
+            var act = () => _controller.Delete(id);
+            act.Should().NotThrow();
+        }
+
+        private Mock<IAsyncCursor<Produto>> CriarCursorMock(List<Produto> produtos)
+        {
+            var cursorMock = new Mock<IAsyncCursor<Produto>>();
+            cursorMock.Setup(c => c.Current).Returns(produtos);
+            cursorMock.SetupSequence(c => c.MoveNext(It.IsAny<CancellationToken>())).Returns(true).Returns(false);
+            cursorMock.SetupSequence(c => c.MoveNextAsync(It.IsAny<CancellationToken>())).ReturnsAsync(true).ReturnsAsync(false);
+            return cursorMock;
+        }
+    }
+}
