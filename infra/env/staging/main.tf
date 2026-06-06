b9fa6e15c692a2b41d4d172c9c03205d74e22140
patrick <patrickpk4@hotmail.com>
2026-06-05 23:22:09 -0300

    edit fmt

--- infra/env/staging/main.tf
+++ infra/env/staging/main.tf
@@ -1,20 +1,20 @@
 ############################# VPC ################################
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.1"
-  name = var.vpc_name
-  cidr = var.cidr_vpc
-  azs             = var.azs_subnets
-  private_subnets = var.cidr_private_sub
-  public_subnets  = var.cidr_public_sub
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "6.6.1"
+  name               = var.vpc_name
+  cidr               = var.cidr_vpc
+  azs                = var.azs_subnets
+  private_subnets    = var.cidr_private_sub
+  public_subnets     = var.cidr_public_sub
   enable_nat_gateway = true
   enable_vpn_gateway = true
-  tags = var.project_tags
+  tags               = var.project_tags
 }
 ########################### EKS #####################################
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  source             = "terraform-aws-modules/eks/aws"
+  version            = "~> 21.0"
   name               = var.kube_name
   kubernetes_version = var.kube_version
   addons = {
@@ -33,11 +33,11 @@ module "eks" {
       })
     }
   }
-  endpoint_public_access  = true
-  endpoint_private_access = true
+  endpoint_public_access                   = true
+  endpoint_private_access                  = true
   enable_cluster_creator_admin_permissions = true
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                                   = module.vpc.vpc_id
+  subnet_ids                               = module.vpc.private_subnets
   eks_managed_node_groups = {
     example = {
       instance_types       = var.instance_types
