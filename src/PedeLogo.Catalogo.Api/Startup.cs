ab8e334f58344c28d85248896691a865127cd4bf
patrick <patrickpk4@hotmail.com>
2026-06-07 19:42:31 -0300

    edit  replicas api

--- src/PedeLogo.Catalogo.Api/Startup.cs
+++ src/PedeLogo.Catalogo.Api/Startup.cs
@@ -49,7 +49,7 @@ namespace PedeLogo.Catalogo.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Version = "v1.0.3",
+                    Version = "v1.0.1",
                     Title = "API para cadastro de Produtos",
                     Description = "API para cadastro de Produtos"
                 });
