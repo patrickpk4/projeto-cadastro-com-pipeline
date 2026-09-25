39bd7a5e124f43e970ef00ec46804e4cbc7560c9
patrick <patrickpk4@hotmail.com>
2026-09-25 20:06:03 -0300

    Enhance CI/CD workflows: update infra_CD.yaml for required inputs and add deployment step in infra_CI.yaml; modify main.tf for EKS module settings

--- infra/main.tf
+++ infra/main.tf
@@ -53,11 +53,11 @@ module "eks" {
   }
 
   # Optional
-  #endpoint_public_access  = false
+  endpoint_public_access  = false
   endpoint_private_access = true
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = false
+  enable_cluster_creator_admin_permissions = true
 
 
   vpc_id     = module.vpc.vpc_id
